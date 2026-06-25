024a1e71624ad802497fcfbf799cd19c6bc2b867
Himanshu Rawat <rawathimanshu2005r@gmail.com>
2026-06-25 07:54:32 +0530

    api commit

--- Frontend/src/Features/interview/Services/interview.api.js
+++ Frontend/src/Features/interview/Services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "https://prepwise-k3rp.onrender.com",
+    baseURL : "https://prepwise-k3rp.onrender.com/",
     withCredentials : true
 })
 
