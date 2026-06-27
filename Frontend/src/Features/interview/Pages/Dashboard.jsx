a2976e4fc70de87e9407f32fb0adc758736acb34
Himanshu Rawat <rawathimanshu2005r@gmail.com>
2026-06-27 10:36:34 +0530

    last changes

--- Frontend/src/Features/interview/Pages/Dashboard.jsx
+++ Frontend/src/Features/interview/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import react from "react";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 export default function Dashboard () {
     const [showDashboard, setShowDashboard] = useState(false);
